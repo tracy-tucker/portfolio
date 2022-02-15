64fcc86c09693ab456c4719862c3c49f7f4b1ae7
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-15 17:32:52 -0600

    Inserting divs and layout to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,17 @@
+import { Link } from 'gatsby'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='navbar'>
+        <div className="nav-center">
+            <div className="nav-header">
+                <Link to='/'>
+                    <img src={logo} alt="Tracy Tucker name" />
+                </Link>
+            </div>
+        </div>
+    </nav>
   )
 }
 
