9e7cb85570e360a5c14002dfa93b8c907ba6bd58
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-15 17:48:13 -0600

    Setting up Navbar toggle button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
-import { Link } from 'gatsby'
 import React from 'react'
+import { Link } from 'gatsby'
+import { FaBars } from 'react-icons/fa'
 import logo from '../assets/coder-news-logo.svg'
 
 const Navbar = () => {
@@ -7,13 +8,19 @@ const Navbar = () => {
     <nav className='navbar'>
         <div className="nav-center">
             <div className="nav-header">
-                <Link to='/'>
+                <Link to='/' className='nav-logo'>
                     <img src={logo} alt="Tracy Tucker name" />
                 </Link>
+                <button className='toggle-btn'>
+                    <FaBars />
+                </button>
             </div>
+            Add Links here
         </div>
     </nav>
   )
 }
 
 export default Navbar
+
+// Set up toggle function
