516cbfd1f820af51257ff54b3f4df81014cda06d
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-16 19:08:57 -0600

    Testing the About page with some content and styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 const about = () => {
   return (
-    <div>about page</div>
+    <Layout>
+      <section className='about-page'>
+        <div className="page-center">
+          <h1>This is the About page.</h1>
+        </div>
+      </section>
+    </Layout>
   )
 }
 
