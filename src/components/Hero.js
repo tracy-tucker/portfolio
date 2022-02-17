6dae804656b8d52ccc78961e77bfb714160b8554
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-17 13:02:27 -0600

    Adding and testing Hero styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import SocialLinks from '../constants/socialLinks'
 
 const Hero = () => {
   return (
-    <section className='intro-section'>
-        <div className="page-center">
+    <section className='hero-comp'>
+        <div className="section-center hero-center">
             <article>
               <h1>I'm Tracy</h1>
+              <h4>My entire career in one sentence</h4>
+              <SocialLinks />
             </article>
             <article>
               image goes here
