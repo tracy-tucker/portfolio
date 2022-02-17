aafbda456a3a0275f1db1eea72586aec6fb3145c
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-17 12:25:53 -0600

    Changing name of Intro to Hero

---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='intro-section'>
+        <div className="page-center">
+            <article>
+              <h1>I'm Tracy</h1>
+            </article>
+            <article>
+              image goes here
+            </article>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout"
-import Intro from '../components/Intro'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
     <Layout>
       I am the Index page.
-      <Intro />
+      <Hero />
     </Layout>
   )
 }
