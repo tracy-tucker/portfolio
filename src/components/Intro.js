2472a567ac727c9d4365d2179ed88f12ae79683f
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-16 19:27:46 -0600

    Inserting test intro into index with test styling

---
+++ src/components/Intro.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <section className='intro-section'>
+        <div className="page-center">
+            <h2>I'm the intro section</h2>
+        </div>
+    </section>
+  )
+}
+
+export default Intro
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout"
+import Intro from '../components/Intro'
 
 export default function Home() {
   return (
     <Layout>
       I am the Index page.
+      <Intro />
     </Layout>
   )
 }
