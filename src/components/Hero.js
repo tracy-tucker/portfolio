6432429ff1c42ca0736b9bd187023c195399b3ce
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-18 07:15:10 -0600

    Inserting profile image, image rename

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SocialLinks from '../constants/socialLinks'
+import profile from '../assets/tracy-tucker-profile.jpg'
 
 const Hero = () => {
   return (
@@ -11,7 +12,7 @@ const Hero = () => {
               <SocialLinks />
             </article>
             <article className='hero-img'>
-              image goes here!
+              <img src={profile} className='hero-photo' alt="Tracy Tucker" />
             </article>
         </div>
     </section>
