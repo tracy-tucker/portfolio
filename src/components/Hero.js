992efb000e491c51d690eb474053d20b974bc9b7
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-18 07:03:29 -0600

    Changing Hero styling, adding profile image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,12 +5,12 @@ const Hero = () => {
   return (
     <section className='hero-comp'>
         <div className="section-center hero-center">
-            <article>
+            <article className='hero-info'>
               <h1>I'm Tracy</h1>
               <h4>My entire career in one sentence</h4>
               <SocialLinks />
             </article>
-            <article>
+            <article className='hero-img'>
               image goes here
             </article>
         </div>
