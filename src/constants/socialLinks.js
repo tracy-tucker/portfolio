56dc851501a52ebf34aa235d0822e5114becb3bc
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-19 11:52:24 -0600

    Adding target to social links

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -10,22 +10,22 @@ const socialLinks = ({styleClass}) => {
   return (
     <ul className={styleClass}>
         <li>
-            <a href="https://www.linkedin.com/in/tracy-tucker/">
+            <a href="https://www.linkedin.com/in/tracy-tucker/" target="_blank">
                 <FaLinkedin className='social-icon'></FaLinkedin>
             </a>
         </li>
         <li>
-            <a href="https://medium.com/@tracy-tucker">
+            <a href="https://medium.com/@tracy-tucker" target="_blank">
                 <FaMedium className='social-icon'></FaMedium>
             </a>
         </li>
         <li>
-            <a href="https://github.com/tracy-tucker">
+            <a href="https://github.com/tracy-tucker" target="_blank">
                 <FaGithubSquare className='social-icon'></FaGithubSquare>
             </a>
         </li>
         <li>
-            <a href="https://twitter.com/mapTracy">
+            <a href="https://twitter.com/mapTracy" target="_blank">
                 <FaTwitterSquare className='social-icon'></FaTwitterSquare>
             </a>
         </li>
