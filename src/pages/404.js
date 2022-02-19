3bfd18c1a4a6c493b54c6b70a454c38d1ac5b06a
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-19 12:06:15 -0600

    Adding Error page content and styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,20 @@
+import { Link } from 'gatsby'
 import React from 'react'
+import Layout from '../components/Layout'
 
 const ErrorPage = () => {
-  return (
-    <div>ErrorPage</div>
-  )
+    return (
+        <Layout>
+          <section className='error-page'>
+            <div className="page-center">
+                <h1>ERROR</h1>
+              <h3>Oops! Something went wrong...</h3>
+              <p>Don't worry, there is a way back to the home page.</p>
+              <Link to='/' className='btn'>Home</Link>
+            </div>
+          </section>
+        </Layout>
+      )
 }
 
 export default ErrorPage
