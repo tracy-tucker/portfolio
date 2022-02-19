42a38af93a3bc265d28d745092100ab331327272
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-19 10:50:11 -0600

    Inserting dummy content for About, adjusting styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { StaticImage } from 'gatsby-plugin-image'
+import { Link } from 'gatsby'
 
 const About = () => {
   return (
     <section className='section'>
         <div className="section-center about-center">
             <article className='about-img'>
-                image goes here
+                <StaticImage
+                    src="../assets/tracy-tucker-in-action.jpg"
+                    className='hero-photo about-photo'
+                    width={350}
+                    height={350}
+                    alt="Tracy Tucker in action"
+                />
             </article>
             <article className='about-info'>
-                about goes here
+                <h2>About</h2>
+                <div className="underline"></div>
+                    <p>Brooklyn farm-to-table raclette bespoke yr celiac roof party vexillologist butcher ramps. Tote bag iceland heirloom retro coloring book lyft. Stumptown crucifix neutra unicorn schlitz marfa waistcoat kitsch. Messenger bag tbh cloud bread portland williamsburg crucifix cray austin snackwave tacos forage dreamcatcher street art. Tote bag humblebrag celiac flannel actually PBR&B twee air plant trust fund meggings enamel pin. Cliche green juice deep v, ethical health goth sustainable 8-bit semiotics salvia.</p>
+                    <Link to='/about' className='btn'>read more</Link>
             </article>
         </div>
     </section>
