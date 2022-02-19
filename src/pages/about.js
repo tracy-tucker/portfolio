25dd232039068881d990e43e994488b4e69a27d5
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-19 12:31:56 -0600

    Testing content and styling for about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,9 +4,10 @@ import Layout from '../components/Layout'
 const about = () => {
   return (
     <Layout>
-      <section className='about-page'>
+      <section className='section about-page'>
         <div className="page-center">
           <h1>This is the About page.</h1>
+          <p>Brooklyn farm-to-table raclette bespoke yr celiac roof party vexillologist butcher ramps. Tote bag iceland heirloom retro coloring book lyft. Stumptown crucifix neutra unicorn schlitz marfa waistcoat kitsch. Messenger bag tbh cloud bread portland williamsburg crucifix cray austin snackwave tacos forage dreamcatcher street art. Tote bag humblebrag celiac flannel actually PBR&B twee air plant trust fund meggings enamel pin. Cliche green juice deep v, ethical health goth sustainable 8-bit semiotics salvia.</p>
         </div>
       </section>
     </Layout>
