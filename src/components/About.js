7c007d054591debef6275c1d6b18e521fc957109
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-19 09:28:19 -0600

    Inserting About into index, styling changes to footer and nav

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about'>
+        <div className="section-center about-center">
+            <article className='about-img'>
+                image goes here
+            </article>
+            <article className='about-info'>
+                about goes here
+            </article>
+        </div>
+    </section>
+  )
+}
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Layout from "../components/Layout"
 import Hero from '../components/Hero'
+import About from "../components/About"
 
 export default function Home() {
   return (
     <Layout>
       I am the Index page.
       <Hero />
+      <About />
     </Layout>
   )
 }
