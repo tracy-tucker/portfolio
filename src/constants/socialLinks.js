530cae31c4a672e046bccbd324c11f4e850baf79
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-19 11:57:06 -0600

    Adding noreferrer to social links

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -10,22 +10,22 @@ const socialLinks = ({styleClass}) => {
   return (
     <ul className={styleClass}>
         <li>
-            <a href="https://www.linkedin.com/in/tracy-tucker/" target="_blank">
+            <a href="https://www.linkedin.com/in/tracy-tucker/" target="_blank" rel="noreferrer">
                 <FaLinkedin className='social-icon'></FaLinkedin>
             </a>
         </li>
         <li>
-            <a href="https://medium.com/@tracy-tucker" target="_blank">
+            <a href="https://medium.com/@tracy-tucker" target="_blank" rel="noreferrer">
                 <FaMedium className='social-icon'></FaMedium>
             </a>
         </li>
         <li>
-            <a href="https://github.com/tracy-tucker" target="_blank">
+            <a href="https://github.com/tracy-tucker" target="_blank" rel="noreferrer">
                 <FaGithubSquare className='social-icon'></FaGithubSquare>
             </a>
         </li>
         <li>
-            <a href="https://twitter.com/mapTracy" target="_blank">
+            <a href="https://twitter.com/mapTracy" target="_blank" rel="noreferrer">
                 <FaTwitterSquare className='social-icon'></FaTwitterSquare>
             </a>
         </li>
