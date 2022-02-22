28ac43a484b6305f9dadada06f0ebe9ebdd2ef33
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-21 19:19:23 -0600

    Working on styling of contact page

--- src/components/Handles.js
+++ src/components/Handles.js
@@ -3,8 +3,18 @@ import { MdEmail } from 'react-icons/md'
 
 const Handles = () => {
   return (
-    <div>
-        <MdEmail />
+    <div className='handle-comp'>
+        {/* <div className='handles'>
+            <MdEmail className='handle-icon'/>
+            <span>TracyTuckerEmail@gmail.com</span>
+        </div>
+        <div className='handles'>
+            <MdEmail className='handle-icon'/>
+            <span>TracyTuckerEmail@gmail.com</span>
+        </div> */}
+        {/* <div className='handle-content'>
+            <span>TracyTuckerEmail@gmail.com</span>
+        </div> */}
     </div>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import Handles from '../components/Handles'
 
 const contact = () => {
   return (
@@ -10,6 +11,8 @@ const contact = () => {
           <div className="underline"></div>
           <h4>Reaching out is just a click away</h4>
         </div>
+        <p>I need to put something here to discuss getting ahold of me</p>
+        <Handles />
       </section>
     </Layout>
   )
