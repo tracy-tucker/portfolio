84864638c74aa9965d81d372a7d67066a14d423c
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-23 07:38:41 -0600

    Correcting contact page layout

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,20 +1,13 @@
 import React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
+import { Link } from 'gatsby'
 import { MdEmail } from 'react-icons/md'
 
 const Info = () => {
   return (
-    <div className='section'>
-      <div className='section-center info-center'>
-        <article className='info-content'>
-          <div>
-          <MdEmail className='info-icon'/> <span>EMAIL</span>
-          </div>
-          <div>
-          <MdEmail className='info-icon'/> <span>EMAIL</span>
-          </div>
-        </article>
-        </div>
-    </div>
+    <section className='section'>
+        {/* info here */}
+    </section>
   )
 }
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,22 +1,36 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { StaticImage } from 'gatsby-plugin-image'
+import { Link } from 'gatsby'
 import Info from '../components/Info'
 
 const contact = () => {
   return (
     <Layout>
-      <section className='section single-page'>
+      <section className='section'>
         <div className='section-title'>
           <h1>Contact me</h1>
           <div className="underline"></div>
-          <h4>Reaching out is just a click away</h4>
         </div>
-        <div className="section-center">
-          <p>Whether you have a question or just want to say hello, my inbox is always open. I'll reply back to you as soon as I am able.</p>
-          <p>Or, feel free to grab my resume instead!</p>
-        </div>
-        <Info />
+        <div className="section-center about-center">
+            <article className='about-img'>
+                <StaticImage
+                    src="../assets/tracy-tucker-in-action.png"
+                    className='hero-photo about-photo'
+                    width={350}
+                    height={350}
+                    alt="Tracy Tucker in action"
+                />
+            </article>
+            <article className='about-info'>
+                <h3>About</h3>
+                {/* <div className="underline"></div> */}
+                    <p>Everyone has their own story how they became a developer. For some, their journey began after years of endless workdays in a career that didn't bring fulfillment. Others may have been fortunate enough that they already knew from the get-go what they wanted to do!</p><p>For me? Mine was a mixture of both. The drive to be a developer was always there for me - I just didn't know it!</p>
+                    <Link to='/about' className='btn'>read more</Link>
+            </article>
+        </div>      
       </section>
+      <Info />
     </Layout>
   )
 }
