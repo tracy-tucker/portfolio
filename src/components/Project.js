be667b36122c7b890ba48b79556ab6fc47778838
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-26 11:08:17 -0600

    Iterating over project in Project comp

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,30 @@
 import React from "react"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-const Project = () => {
-  return <div>Project</div>
+const Project = project => {
+  const { id, title, image, sourceCode, description, content, gitHub } = project
+  const pathToImage = getImage(image)
+  const desc = description.description
+  const { stack, tags } = content
+
+  return (
+    <div>
+      <div key={id}>
+        <h3>{title}</h3>
+        <GatsbyImage image={pathToImage} alt={title} />
+        <a href={sourceCode} target="_blank" rel="noreferrer">
+          Source Code Link
+        </a>
+        <p>{desc}</p>
+        {stack.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+        <a href={gitHub} target="_blank" rel="noreferrer">
+          GitHub Link
+        </a>
+      </div>
+    </div>
+  )
 }
 
 export default Project
