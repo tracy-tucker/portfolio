bab6db7a6bf23db3e66e0c125290a83335cb1613
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-26 12:05:07 -0600

    Index page requesting projects from contentful

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,47 @@ import React from "react"
 import Layout from "../components/Layout"
 import Hero from "../components/Hero"
 import About from "../components/About"
+import Projects from "../components/Projects"
+import { graphql } from "gatsby"
 
-export default function Home() {
+const Home = ({ data }) => {
+  const {
+    allContentfulProject: { nodes: projects },
+  } = data
+
   return (
     <Layout>
       <Hero />
       <About />
+      <Projects projects={projects} />
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allContentfulProject(filter: { featured: { eq: true } }) {
+      nodes {
+        id
+        title
+        image {
+          gatsbyImageData
+        }
+        sourceCode
+        description {
+          description
+        }
+        content {
+          id
+          stack
+          tags
+        }
+        gitHub
+      }
+    }
+  }
+`
+
+export default Home
 
 // Make data request on this page
