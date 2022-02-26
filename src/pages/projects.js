f470117eb1c1bea3f4e702a7c7d6a93b58093d27
Tracy Tucker <tracytuckeremail@gmail.com>
2022-02-26 08:58:54 -0600

    Creating projects page layout with starting content

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react"
+import AllProjects from "../components/AllProjects"
+import Layout from "../components/Layout"
 
 const projects = () => {
   return (
-    <div>projects page</div>
+    <Layout>
+      <section className="section height">
+        <div className="section-title">
+          <h1>Projects</h1>
+          <div className="underline"></div>
+          <div className="section-center about-center">
+            here are the projects
+            <AllProjects />
+          </div>
+        </div>
+      </section>
+    </Layout>
   )
 }
 
 export default projects
+
+// Make data request on this page
+// Pass over to Projects
